9f0114ce2b10d77cba4c3cfd6a8dd6277aaaabd5
dansaa8 <dansaa8@protonmail.com>
2024-12-22 23:27:42 +0100

    Add project files.

---
+++ BlazorTodoApp.Shared/Domain/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorTodoApp.Shared.Domain
+{
+    public class Todo
+    {
+        public int TodoId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
---
+++ BlazorTodoApp/Data/TodoAppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BlazorTodoApp.Shared.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorTodoApp.Data
+{
+    public class TodoAppDbContext : DbContext
+    {
+        public TodoAppDbContext(DbContextOptions<TodoAppDbContext> options) : base(options) { }
+
+        public DbSet<Todo> Todos { get; set; }
+
+    }
+}
