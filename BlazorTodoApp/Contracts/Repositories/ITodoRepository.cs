d062560cb5ea6221ff1d7e1ec0a42bfcae458e17
dansaa8 <dansaa8@protonmail.com>
2024-12-22 23:55:09 +0100

    Add contracts for TodoRepository and TodoService

---
+++ BlazorTodoApp/Contracts/Repositories/ITodoRepository.cs
@@ -0,0 +1,13 @@
+﻿using BlazorTodoApp.Shared.Domain;
+
+namespace BlazorTodoApp.Contracts
+{
+    public interface ITodoRepository
+    {
+        Task<IEnumerable<Todo>> GetAllTodos();
+        Task<Todo> GetTodoById(int todoId);
+        Task<Todo> AddTodo(Todo todo);
+        Task<Todo> UpdateTodo(Todo todo);
+        Task DeleteTodo(int todoId);
+    }
+}
---
+++ BlazorTodoApp/Contracts/Services/ITodoService.cs
@@ -0,0 +1,13 @@
+﻿using BlazorTodoApp.Shared.Domain;
+
+namespace BlazorTodoApp.Contracts.Services
+{
+    public interface ITodoService
+    {
+        Task<IEnumerable<Todo>> GetAllTodos();
+        Task<Todo> GetTodoDetails(int todoId);
+        Task<Todo> AddTodo(Todo todo);
+        Task UpdateTodo(Todo todo);
+        Task DeleteTodo(int todoId);
+    }
+}
