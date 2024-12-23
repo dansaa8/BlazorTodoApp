6dd40e67c703cc4dbc77d56f38fdc1ad76337f1b
dansaa8 <dansaa8@protonmail.com>
2024-12-23 01:26:54 +0100

    Add TodoRepo and TodoService into DI-container, also setup mssqlserver

--- BlazorTodoApp/Program.cs
+++ BlazorTodoApp/Program.cs
@@ -1,10 +1,23 @@
 using BlazorTodoApp.Components;
+using BlazorTodoApp.Contracts;
+using BlazorTodoApp.Contracts.Services;
+using BlazorTodoApp.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDbContextFactory<TodoAppDbContext>(options =>
+options.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnection"]));
+
+builder.Services.AddScoped<ITodoService, ITodoService>();
+builder.Services.AddScoped<ITodoRepository, ITodoRepository>();
+
+//builder.Services.AddScoped<ApplicationState>(); // Försök få in appstate
+//builder.Services.AddSingleton(IHttpContextAccessor, HttpContextAccessor()); Wut is this?
 
 var app = builder.Build();
 
